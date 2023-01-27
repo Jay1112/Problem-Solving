59a60e12db4eebc9f8c69950301e07554371b243
Jay Patel <38917175+Jay1112@users.noreply.github.com>
2023-01-27 23:55:03 +0530

    Add files via upload

---
+++ Arrays/06-Array-Search-In-Rotated-And-Sorted-Array.cpp
@@ -0,0 +1,36 @@
+// Search in Rotated and Sorted Array -> LeetCode
+
+// Time  Complexity : O(log(n))
+// Space Complexity : O(1)
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0 ; 
+        int high = nums.size() - 1; 
+
+        while(low <= high){
+            int mid = (low + high) / 2;
+
+            if(nums[mid] == target){
+                return mid;
+            }
+
+            if(nums[low] <= nums[mid]){
+                if(target >= nums[low] && target <= nums[mid]){
+                    high = mid - 1 ;
+                }else{
+                    low = mid + 1;
+                }
+            }else{
+                if(target >= nums[mid+1] && target <= nums[high]){
+                    low = mid + 1 ;
+                }else{
+                    high = mid - 1 ;
+                }
+            }
+        }
+
+        return -1;
+    }
+};
